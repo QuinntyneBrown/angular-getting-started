a2a1fba8a926f9f7164f27a5d5b893b450ca3667
Quinn Brown <Quinn Brown>
2015-11-13 04:18:29 -0500

    feat(home): make homeComponent more interesting

--- dist/app.js
+++ dist/app.js
@@ -81,7 +81,25 @@ angular.module("app", ["ngX.components"]).config(["$routeProvider", function ($r
     "use strict";
 
     function HomeComponent() {
+        var self = this;
 
+        self.videos = [
+            {
+                id: 'VdxTkx4qnKg',
+                name: 'angular_hackathon'
+            },
+            {
+                id: 'cgSJlAOmQE4',
+                name: 'technology_radar'
+            },
+            {
+                id: '4YmnbGoh49U',
+                name: 'sngulsr_2_better_concepts_less_code'
+            }
+        ];
+
+        
+        return self;
     }
 
     ngX.Component({
--- src/app/components/home.component.js
+++ src/app/components/home.component.js
@@ -3,7 +3,25 @@
     "use strict";
 
     function HomeComponent() {
+        var self = this;
 
+        self.videos = [
+            {
+                id: 'VdxTkx4qnKg',
+                name: 'angular_hackathon'
+            },
+            {
+                id: 'cgSJlAOmQE4',
+                name: 'technology_radar'
+            },
+            {
+                id: '4YmnbGoh49U',
+                name: 'sngulsr_2_better_concepts_less_code'
+            }
+        ];
+
+        
+        return self;
     }
 
     ngX.Component({
