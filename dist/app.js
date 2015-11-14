62e81e99cbca2578010cdd1718c63d9f1cc69f9a
Quinn Brown <Quinn Brown>
2015-11-14 17:44:47 -0500

    feat(config): build routes using ngX func

--- dist/app.js
+++ dist/app.js
@@ -1,32 +1,7 @@
-angular.module("app", ["ngX.components"]).config(["$routeProvider", function ($routeProvider) {
-
-    var routes = JSON.parse(ngX.getFromUrlSync({ url: "routes.json " }));
-
-    for (var i = 0; i < routes.length; i++) {
-        $routeProvider.when(routes[i].when, routes[i].config);
-    }
-
-    //var routes = ngX.getFromUrlSync({ url: "routes.json " });
-
-    //$routeProvider.when("/", {
-    //    componentName: "homeComponent",
-    //    authorizationRequired: true
-    //});
-
-    //$routeProvider.when("/about", {
-    //    componentName: "aboutComponent",
-    //    authorizationRequired: true
-    //});
-
-    //$routeProvider.when("/login", {
-    //    componentName: "loginComponent",
-    //    authorizationRequired: false
-    //});
-}]).run(["$location", "$rootScope", "securityManager", function ($location, $rootScope, securityManager) {
-
+angular.module("app", ["ngX.components"]).config(["$routeProvider", function ($routeProvider) {    
+    $routeProvider.buildFromUrl({ url: "routes.json" });
+}]).run(["$rootScope", function ($rootScope) {
     $rootScope.title = "Getting Started";
-
-
 }]);
 (function() {
 
